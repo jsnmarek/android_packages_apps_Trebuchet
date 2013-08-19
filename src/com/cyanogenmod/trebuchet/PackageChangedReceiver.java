LawnchairLauncher/lawnchair

Cache widget previews in a DB
- Smoother All Apps scroll performance

Change-Id: Id2d31a45e71c63d05a46f580667ad94403730616

src/com/android/launcher2/PackageChangedReceiver.java
@@ -1,0 +1,18 @@
+package com.android.launcher2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class PackageChangedReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        final String packageName = intent.getData().getSchemeSpecificPart();
+
+        if (packageName == null || packageName.length() == 0) {
+            // they sent us a bad intent
+            return;
+        }
+        WidgetPreviewLoader.removeFromDb(context, packageName);
+    }
+}

jsnmarek/android_packages_apps_Trebuchet

src/com/cyanogenmod/trebuchet/PackageChangedReceiver.java
@@ -1,0 +1,18 @@
+package com.cyanogenmod.trebuchet;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class PackageChangedReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        final String packageName = intent.getData().getSchemeSpecificPart();
+
+        if (packageName == null || packageName.length() == 0) {
+            // they sent us a bad intent
+            return;
+        }
+        WidgetPreviewLoader.removeFromDb(context, packageName);
+    }
+}